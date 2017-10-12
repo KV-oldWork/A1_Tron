e0d7ca34b45ee220edc61a5b1a590cdee46e5d24
kylevincent <kyle.vincent@my.jcu.edu.au>
2017-10-12 14:18:01 +1000

    Finished grid, player can now move bike

--- program_files/Bike.java
+++ program_files/Bike.java
@@ -21,6 +21,26 @@ public class Bike
         g.setColor(Color.BLACK);
         g.fillRect(xPos * width, yPos * height, width, height);
         g.setColor(Color.GREEN);
-        g.fillRect(xPos * width + 2, yPos *height + 2, width, height);
+        g.fillRect(xPos * width + 2, yPos *height + 2, width - 4, height - 4);
+    }
+
+    public int getxPos()
+    {
+        return xPos;
+    }
+
+    public void setxPos(int xPos)
+    {
+        this.xPos = xPos;
+    }
+
+    public int getyPos()
+    {
+        return yPos;
+    }
+
+    public void setyPos(int yPos)
+    {
+        this.yPos = yPos;
     }
 }
--- program_files/Screen.java
+++ program_files/Screen.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 public class Screen extends JPanel implements Runnable
 {
@@ -7,20 +10,82 @@ public class Screen extends JPanel implements Runnable
     private Thread thread;
     private boolean running = false;
 
+    private Bike b;
+    private ArrayList<Bike> bikesBody;
+
+    private int xPos = 10, yPos = 40;
+    private int size = 5;
+
+    private boolean up = true, down = false, left = false, right = false;
+
+    private int ticks = 0;
+
+    private userKey userKey;
+
     public Screen()
     {
+        setFocusable(true);
+        userKey = new userKey();
+        addKeyListener(userKey);
+
         setPreferredSize(new Dimension(Width, Height));
+
+        bikesBody = new ArrayList<Bike>();
 
         start();
     }
 
     public void tick()
     {
-        System.out.println("Running");
+        if(bikesBody.size() == 0)
+        {
+            b = new Bike(xPos, yPos, 10);
+            bikesBody.add(b);
+        }
+
+        /// stops the game if the bike collides with itself
+        for(int i = 0; i < bikesBody.size(); i++)
+        {
+            if(xPos == bikesBody.get(i).getxPos() && yPos == bikesBody.get(i).getyPos())
+            {
+                if(i != bikesBody.size() - 1)
+                {
+                    stop();
+                }
+            }
+        }
+
+        /// stops the game if the bike goes out of bounds
+        if(xPos < 1 || xPos > 78 || yPos < 1 || yPos > 78)
+        {
+            stop();
+        }
+
+        ticks++;
+        if(ticks > 400000)
+        {
+            if(right) xPos++;
+            if(left) xPos--;
+            if(up) yPos--;
+            if(down) yPos++;
+
+            ticks = 0;
+
+            b = new Bike(xPos, yPos, 10);
+            bikesBody.add(b);
+
+            if(bikesBody.size() > size)
+            {
+                bikesBody.remove(0);
+            }
+        }
     }
 
     public void paint(Graphics g)
     {
+        g.clearRect(0, 0, Width, Height);
+
+        ///draws a number of lines over the screen, for testing purposes
         g.setColor(Color.BLACK);
         for(int i = 0; i < Width / 10; i++)
         {
@@ -29,6 +94,11 @@ public class Screen extends JPanel implements Runnable
         for(int i = 0; i < Height / 10; i ++)
         {
             g.drawLine(0, i * 10, Width, i * 10);
+        }
+
+        for(int i = 0; i < bikesBody.size(); i ++)
+        {
+            bikesBody.get(i).draw(g);
         }
     }
 
@@ -41,7 +111,12 @@ public class Screen extends JPanel implements Runnable
 
     public void stop()
     {
-
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     public void run()
@@ -52,4 +127,73 @@ public class Screen extends JPanel implements Runnable
             repaint();
         }
     }
+    private class userKey implements KeyListener
+    {
+        public void keyTyped(KeyEvent e)
+        {
+        }
+
+        public void keyPressed(KeyEvent e)
+        {
+            int key = e.getKeyCode();
+            if(up == true)
+            {
+                if(key == KeyEvent.VK_RIGHT)
+                {
+                    up = false;
+                    down = false;
+                    left = false;
+                    right = true;
+                }
+                if(key== KeyEvent.VK_LEFT)
+                {
+                    down = false;
+                    up = false;
+                    left = true;
+                    right = false;
+                }
+            }
+            if(left == true)
+            {
+                if(key == KeyEvent.VK_RIGHT)
+                {
+                    up = true;
+                    down = false;
+                    left = false;
+                    right = false;
+                }
+                if(key== KeyEvent.VK_LEFT)
+                {
+                    down = false;
+                    up = false;
+                    left = true;
+                    right = false;
+                }
+            }
+
+            if(key == KeyEvent.VK_UP && !down)
+            {
+                up = true;
+                down = false;
+                left = false;
+                right = false;
+            }
+
+            if(key == KeyEvent.VK_DOWN && !up)
+            {
+                up = false;
+                down = true;
+                left = false;
+                right = false;
+
+            }
+
+
+        }
+
+        public void keyReleased(KeyEvent e)
+        {
+        }
+    }
+
 }
